152ed928fc8a8710ed9c9f21f92b91e889b35468
aptaq <48678992+aptaq@users.noreply.github.com>
2019-06-21 13:16:56 +0200

    nowe hehe

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+   <meta charset="utf-8">
+   <title>Struktura strony za pomocą znaków semantycznych</title>
+   <link rel="stylesheet" href="style.css" type ="text/css"/>
+   <body>
+      <nav>
+        <ul>
+          <li>Opcja</li>
+          <li>
+            <a href="logowanie.php">Zaloguj się
+            </a> </li>
+        </ul>
+      </nav>
+      <div id="container">
+        <header>
+          <h1>HEADER: Typical test site</h1>
+        </header>
+        <div class="wrapper">
+          <aside>
+            <h2>ASIDE</h2>
+            <ul>
+              <li>links</li>
+              (...)
+            </ul>
+          </aside>
+          <main>
+            <h2>MAIN</h2>
+            <section>
+              <h2>SECTION</h2>
+              <article>
+                <h3>ARTICLE: Lorem ipsum</h3>
+                <p>(...)</p>
+                <p><a href="article.html">read more...</a></p>
+              </article>
+              <article>
+                <h3>ARTICLE: Lorem ipsum</h3>
+                <p>(...)</p>
+                <p><a href="article.html">read more...</a></p>
+              </article>
+            </section>
+          </main>
+        </div>
+        <footer class="clear">
+          <h3>Typical test site</h3>
+        </footer>
+      </div>
+  </body>
---
+++ logowanie.php
@@ -0,0 +1,7 @@
+<form action = "zaloguj.php" method="post">
+
+   Login: <br /> <input type="text" name="login"/><br/>
+   Hasło: <br /> <input type="password" name="haslo"/><br/><br/>
+   <input type="submit" value="Zaloguj sie"/><br/>
+
+ </form> 
